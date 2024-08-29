09524ef78b07c284f93906474ceb787386838e29
AluFu <0hp3261131m102u@ezweb.ne.jp>
2024-08-29 19:23:25 +0900

    add:APIルーティングを設定

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -8,7 +8,16 @@ def create_app():
     initialize_db(app)
 
     # 他のブループリントや設定の登録
-    
+    # ユーザー情報に関するAPI
+    from .view.user_view.user_select import user_select
+    from .view.user_view.user_insert import user_insert
+    from .view.user_view.user_update import user_update
+    from .view.user_view.user_delete import user_delete
+
+    app.register_blueprint(user_select, url_prefix="/api/user/select")
+    app.register_blueprint(user_insert, url_prefix="/api/user/insert")
+    app.register_blueprint(user_update, url_prefix="/api/user/update")
+    app.register_blueprint(user_delete, url_prefix="/api/user/delete")
 
     return app
 
