2ac33ad7fb8d0604cc20396c3d47e0c0fea0b294
AluFu <0hp3261131m102u@ezweb.ne.jp>
2024-08-31 06:59:31 +0900

    update:ファイル名の変更+ファイル名変更に影響する部分を変更

--- app/api/view/tag_view/tags_find.py
+++ app/api/view/tag_view/tags_find.py
@@ -2,37 +2,37 @@ from flask import Blueprint, jsonify, abort
 from api.models import TagModel
 
 # ルーティング設定
-tag_select = Blueprint("tag_select", __name__)
+tags_find = Blueprint("tag_find", __name__)
 
-@tag_select.route("", methods=["POST"])
-def select_tag():
+@tags_find.route("", methods=["POST"])
+def find_tags():
     # 現在の月を取得
     import datetime
     month = datetime.datetime.now().month
 
     # 季節を判定
     if month >= 3 and month <= 5:
-        tag_season = "spring"
+        tags_season = "spring"
     elif month >= 6 and month <= 8:
-        tag_season = "summer"
+        tags_season = "summer"
     elif month >= 9 and month <= 11:
-        tag_season = "autumn"
+        tags_season = "autumn"
     else:
-        tag_season = "winter"
+        tags_season = "winter"
 
     # TagModelクラスのインスタンスを生成
-    tag_model = TagModel()
+    tags_model = TagModel()
 
     try:
         # 該当する季節のタグ情報を取得
-        tag_data_list = tag_model.find_tag(tag_season)
+        tags_data_list = tags_model.find_tag(tags_season)
         # _idを文字列に変換
-        for tags_data in tag_data_list:
+        for tags_data in tags_data_list:
             tags_data["_id"] = str(tags_data["_id"])
 
         return jsonify({
             "code": 200,
-            "tags": tag_data_list
+            "tags": tags_data_list
         })
     except Exception as e:
         abort(500, f"Error occurred while selecting tags: {e}")
