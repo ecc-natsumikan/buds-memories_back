5918aa64606756abb6662a3f8d8e968d4147cb21
AluFu <0hp3261131m102u@ezweb.ne.jp>
2024-08-31 09:08:27 +0900

    add:model配下の GrowItemModelのインポートパスを設定

--- app/api/models/__init__.py
+++ app/api/models/__init__.py
@@ -3,3 +3,4 @@ from .tag_model import TagModel
 from .post_model import PostModel
 from .user_post_model import UserPostModel
 from .like_post_model import LikePostModel
+from .grow_item_model import GrowItemModel
